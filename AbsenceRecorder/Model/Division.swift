128f4116445cac6caa0438118f8b65d5a7407651
HarrySibson <94445782+HarrySibson@users.noreply.github.com>
2022-02-01 17:46:33 +0000

    Adding basic model for students and divisions

---
+++ AbsenceRecorder/Model/Division.swift
@@ -0,0 +1,17 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by Harry Sibson on 01/02/2022.
+//
+
+import Foundation
+
+class Division {
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String){
+        self.code = code
+    }
+}
