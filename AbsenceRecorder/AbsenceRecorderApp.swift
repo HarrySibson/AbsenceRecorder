bc86e215cd1ad1bebece0cdcd32076c92b77268b
HarrySibson <94445782+HarrySibson@users.noreply.github.com>
2022-02-01 17:20:58 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Harry Sibson on 01/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AbsenceRecorder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AbsenceRecorder
+//
+//  Created by Harry Sibson on 01/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
