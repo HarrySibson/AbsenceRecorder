655f7108017f14899ae3eb80f3342d72e42327d2
HarrySibson <94445782+HarrySibson@users.noreply.github.com>
2022-02-02 16:53:19 +0000

    Finished Absence Recorder 2

--- AbsenceRecorder/AbsenceRecorderApp.swift
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct AbsenceRecorderApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(divisions: Division.examples)
         }
     }
 }
+
+
